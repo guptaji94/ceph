bf23b5f052ce113ffe77abb8ebf7f837bb14ec3d
spencer-melnick <smelnick97@gmail.com>
2018-09-28 19:55:59 -0400

    Updating test script

--- src/librbd/cache/test.sh
+++ src/librbd/cache/test.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
 
+# Attempt to stop any running ceph instances
 ../src/stop.sh
+# Remove potentially write protected files in output
+sudo rm -rf out/2018*
+
+# Build
 make -j48 librbd
 make -j48
+
+# Restart ceph
 ../src/vstart.sh -n
 bin/ceph osd pool create rbd 75
 bin/rbd pool init
+
+# Import existing image
 bin/rbd import --export-format 2 StartTesting
-sudo bin/rbd device map -t nbd StartTesting
-sudo mount -osync /dev/nbd0 /mnt/testimage
+
+# Map rbd image and grab device name
+nbd_device = $(sudo bin/rbd device map -t nbd StartTesting | grep -e "nbd[0-9]")
+
+# Mount device and run tests
+sudo mount -osync "/dev/$nbd_device" /mnt/testimage
+cd /mnt/testimage/
+dd if=/dev/zero of=zeros.txt obs=1024 count=240
+./testDa
+cd -
+
+# Unmount
+sudo umount /dev/$nbd_device
